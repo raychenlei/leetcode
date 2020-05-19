0c52993e024bf5896a04c832fa04cc97292ce4f5
raychenlei <raychenlei@163.com>
2020-05-20 02:17:02 +0800

    leetcode 70 爬楼梯

--- src/main/java/leetcode/editor/cn/LeetCode70ClimbingStairs.java
+++ src/main/java/leetcode/editor/cn/LeetCode70ClimbingStairs.java
@@ -1,4 +1,3 @@
-
 //假设你正在爬楼梯。需要 n 阶你才能到达楼顶。 
 //
 // 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ 
@@ -25,17 +24,22 @@
 // Related Topics 动态规划
 
 package leetcode.editor.cn;
-public class ClimbingStairs {
+public class LeetCode70ClimbingStairs {
     public static void main(String[] args) {
-        Solution solution = new ClimbingStairs().new Solution();
+        Solution solution = new LeetCode70ClimbingStairs().new Solution();
     }
     //leetcode submit region begin(Prohibit modification and deletion)
-
     class Solution {
-    public int climbStairs(int n) {
-        return 1;
+        public int climbStairs(int n) {
+            int[] dp = new int[n + 1];
+            dp[0] = 1;
+            dp[1] = 1;
+            for(int i = 2; i <= n; i ++){
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+            return dp[n];
+        }
     }
-}
 //leetcode submit region end(Prohibit modification and deletion)
 
 }
